059aaba540db46d46e496221dea980e89cd25cc5
Andreas Schneider <andreas.schneider@vaimo.com>
2022-05-04 16:45:01 +0100

    Added es6 to config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.export = {
     extends: ['airbnb-base'],
     plugins: ['import'],
+    env: {
+        "es6": true
+    },
     rules: {
         radix: 0
     }
--- react.js
+++ react.js
@@ -1,6 +1,9 @@
 module.export = {
     extends: ['airbnb', 'airbnb/hooks'],
     plugins: ['import'],
+    env: {
+        "es6": true
+    },
     rules: {
         radix: 0
     }
